b339e6e96f41ed859238776f9f31e7ea62d8919a
SebastianLorenzano <selorenzano3@gmail.com>
2023-12-15 20:44:18 +0100

    Create SQLQuery2.sql

---
+++ Trabajos/SQLQuery2.sql
@@ -0,0 +1,17 @@
+SELECT codCliente, nombre_cliente
+  FROM CLIENTES
+ WHERE  codCliente%2=0;
+
+ SELECT DISTINCT pais
+   FROM CLIENTES
+
+ SELECT codCliente
+   FROM CLIENTES
+  ORDER BY limite_credito DESC
+  
+
+  SELECT *
+    FROM CLIENTES
+   WHERE ciudad NOT IN ('pEPE')
+
+   USE JARDINERIA
