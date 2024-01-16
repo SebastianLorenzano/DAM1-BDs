dc858a5c95f484e2c855b94e218b31845b32e830
SebastianLorenzano <selorenzano3@gmail.com>
2024-01-16 16:51:51 +0100

    Update SQL16-01-24.sql

--- Trabajos/SQL16-01-24.sql
+++ Trabajos/SQL16-01-24.sql
@@ -20,4 +20,28 @@ SELECT p.*, c.nombre_cliente
         EMPLEADOS j
   WHERE e.codEmplJefe = j.codEmpleado
 
-  
+SELECT pe.codPedido, pe.fecha_pedido, 
+       de.codProducto, de.cantidad, 
+       pr.nombre, 
+       ca.nombre
+  FROM  PEDIDOS pe,
+        DETALLE_PEDIDOS de,
+        PRODUCTOS pr,
+        CATEGORIA_PRODUCTOS ca
+ WHERE pe.codPedido = de.codPedido 
+   AND de.codProducto = pr.codProducto
+   AND pr.codCategoria = ca.codCategoria
+
+SELECT *
+  FROM PEDIDOS
+
+SELECT *
+  FROM DETALLE_PEDIDOS
+
+SELECT *
+  FROM PRODUCTOS
+
+
+SELECT e.codEmpleado, e.nombre
+  FROM EMPLEADOS e LEFT JOIN EMPLEADOS j
+    ON e.codEmplJefe = j.codEmplJefe
