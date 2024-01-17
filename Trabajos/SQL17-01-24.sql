4ef1dc6de89baa0b5c3edb2d993673e54b2c57d4
SebastianLorenzano <selorenzano3@gmail.com>
2024-01-17 16:03:51 +0100

    Update SQL17-01-24.sql

--- Trabajos/SQL17-01-24.sql
+++ Trabajos/SQL17-01-24.sql
@@ -11,3 +11,9 @@ SELECT c.codCliente, c.nombre_cliente, COUNT(p.codCliente)
   FROM  CLIENTES c LEFT JOIN PAGOS p
     ON p.codCliente = c.codCliente
  GROUP BY c.codCliente, c.nombre_cliente
+
+SELECT c.codCliente, c.nombre_cliente,
+       COUNT(p.codCliente) Numpagos,
+       ISNULL(SUM(p.importe_pago), 0) TotalPagos
+  FROM CLIENTES c LEFT JOIN PAGOS p
+    ON p.codCliente = c.codCliente
